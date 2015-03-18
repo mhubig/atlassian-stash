188a5740e979e4e901bb5643fe50bb801c97dceb
Aaron Szerencses <as@imko.de>
2015-03-18 09:22:42 +0100

    Initial commit.
    Signed-off-by: Aaron Szerencses <as@imko.de>

---
+++ docker/postgres/setup-stash-db.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+echo "******CREATING STASH DATABASE*****"
+gosu postgres postgres --single <<- EOSQL
+   CREATE DATABASE stash WITH ENCODING 'UNICODE' LC_COLLATE 'C' LC_CTYPE 'C' \
+       TEMPLATE template0;
+   CREATE USER stash;
+   GRANT ALL PRIVILEGES ON DATABASE stash to stash;
+EOSQL
+echo ""
+
+{ echo; echo "host stash stash 0.0.0.0/0 trust"; } >> "$PGDATA"/pg_hba.conf
+
+if [ -r '/tmp/stash.dump' ]; then
+    echo "**IMPORTING STASH DATABASE BACKUP**"
+    gosu postgres postgres &
+    SERVER=$!; sleep 2
+    gosu postgres psql stash < /tmp/stash.dump
+    kill $SERVER; wait $SERVER
+    echo "**STASH DATABASE BACKUP IMPORTED***"
+fi
+
+echo "******STASH DATABASE CREATED******"
